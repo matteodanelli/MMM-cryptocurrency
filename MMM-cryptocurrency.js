a19506a2405c77db8925be4ee1efa41d59e01e0f
Matteo Danelli <matteo.danelli@mindtek.it>
2017-05-15 21:20:44 +0200

    Improved css. Bug fix. Added IT

--- MMM-cryptocurrency.js
+++ MMM-cryptocurrency.js
@@ -36,12 +36,9 @@ Module.register("MMM-cryptocurrency", {
         var wrapper = document.createElement("table");
         wrapper.className = 'small mmm-cryptocurrency';
         var data = this.result;
-
         var tableHead = document.createElement("tr");
         tableHead.className = 'header-row';
 
-        // Using the MM Translate function to translate the strings
-        // just add another translation key to extend the table head
         var tableHeadValues = [
             this.translate("CURRENCY"),
             this.translate('PRICE'),
@@ -51,93 +48,75 @@ Module.register("MMM-cryptocurrency", {
         for (var thCounter = 0; thCounter < tableHeadValues.length; thCounter++) {
             var tableHeadSetup = document.createElement("th");
             tableHeadSetup.innerHTML = tableHeadValues[thCounter];
-
             tableHead.appendChild(tableHeadSetup);
         }
-
         wrapper.appendChild(tableHead);
 
-
         for (var trCounter = 0; trCounter < data.length; trCounter++) {
-
             var oneCurrency = data[trCounter];
-
             var trWrapper = document.createElement("tr");
-
+            trWrapper.className = 'currency';
             if (this.config.displayLongNames) {
                 var name = oneCurrency.name;
             } else {
                 name = oneCurrency.symbol;
             }
-
             var rightCurrencyFormat = this.config.conversion.toLowerCase();
-
             // rounding the price and adds the currency string
-            var formattedPrice = Math.round(oneCurrency['price_'+rightCurrencyFormat])+' '+this.config.conversion;
-
+            var formattedPrice = Math.round(oneCurrency['price_'+rightCurrencyFormat] * 100) / 100+' '+this.config.conversion;
             // add another value to this array to add additional data
             var tdValues = [
                 name,
                 formattedPrice,
                 oneCurrency.percent_change_24h+'%'
             ];
-
             for (var c = 0; c < tdValues.length; c++) {
                 var tdWrapper = document.createElement("td");
                 tdWrapper.innerHTML = tdValues[c];
                 trWrapper.appendChild(tdWrapper);
             }
-
             wrapper.appendChild(trWrapper);
         }
-
         return wrapper;
     },
 
     socketNotificationReceived: function (notification, payload) {
         if (notification === "got_result") {
-
             this.result = this.getWantedCurrencies(this.config.currency, payload);
             this.updateDom();
         }
     },
 
     /**
-     * Returns the configured currencies
+     * Returns configured currencies
      *
-     * @param wantedCurrencies
+     * @param chosenCurrencies
      * @param apiResult
      * @returns {Array}
      */
-    getWantedCurrencies: function (wantedCurrencies, apiResult) {
-
-        // get wanted Currencies from config and loop trough them
-
+    getWantedCurrencies: function (chosenCurrencies, apiResult) {
         var filteredCurrencies = [];
-        // loop trough whole api result
-        for (var i = 0; i < apiResult.length; i++){
-            var singleCurrency = apiResult[i];
-            // loop trough currency's specified in config
-            for (var c = 0; c < wantedCurrencies.length; c++){
-                if(singleCurrency.id == wantedCurrencies[c]){
-                    // add them to our wanted list
-                    filteredCurrencies.push(singleCurrency)
+        for (var i = 0; i < chosenCurrencies.length; i++){
+            for (var j = 0; j < apiResult.length; j++){
+                var userCurrency = chosenCurrencies[i];
+                var remoteCurrency = apiResult[j];
+                if (userCurrency == remoteCurrency.id) {
+                    filteredCurrencies.push(remoteCurrency);
                 }
             }
         }
-
         return filteredCurrencies;
-
     },
 
     /**
      * Load translations files
-     * @returns {{en: string, de: string}}
+     * @returns {{en: string, de: string, it: string}}
      */
     getTranslations: function () {
         return {
             en: "translations/en.json",
-            de: "translations/de.json"
+            de: "translations/de.json",
+            it: "translations/it.json"
         };
     },
 
